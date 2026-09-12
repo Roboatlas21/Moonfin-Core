5eec6120e1b82275630798a0b181cb6e67f03c52
Roboatlas21 <silva.chris423@gmail.com>
2026-09-12 20:14:38 +0000

    Exclude Media3 Cast from diagnostic build

--- build-android.sh
+++ build-android.sh
@@ -69,6 +69,7 @@ cd "$REPO_ROOT"
 MEDIA3_DIR="$REPO_ROOT/android/.media3"
 rm -rf "$MEDIA3_DIR"
 git clone --depth 1 --branch 1.10.1 https://github.com/androidx/media.git "$MEDIA3_DIR"
+sed -i "/include modulePrefix + 'lib-cast'/,+1d" "$MEDIA3_DIR/core_settings.gradle"
 git -C "$MEDIA3_DIR" apply "$REPO_ROOT/patches/media3-1.10.1-hls-trace.patch"
 
 echo "Cleaning previous Flutter outputs..."
